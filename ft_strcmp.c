353a2314020389d0546ea3d2f7b30e696882ee09
Sophia Koulen <sophia.koulen@outlook.com>
2023-03-01 11:04:01 +0100

    tested ft_strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 14:54:36 by skoulen           #+#    #+#             */
-/*   Updated: 2023/02/28 14:55:23 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/03/01 10:33:32 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	(void)s1;
-	(void)s2;
-	return (0);
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
